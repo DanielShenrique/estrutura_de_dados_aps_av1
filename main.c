a18009ca46433133380f8cf8c42cd6c3752227cd
Natan Xavier <natanxaviersiquera@gmail.com>
2020-09-17 21:21:53 -0300

    nome das variaveis e tratamento do menu

--- main.c
+++ main.c
@@ -1,95 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAX 10
 
 typedef struct
 {
-    int key;
-} Key;
+    int chave;
+} Chave;
 typedef struct
 {
-    Key key;
+    Chave chave;
 
-    int content;
-} Element;
+    int Elemento;
+} Elemento;
 typedef struct
 {
-    int ini;
-    int end;
-    int cor;
-    int num;
+    int inicio; // inicio da lista
+    int final;	// Final da lista
+    int no;		// posisao atual de busca do vetor
+    int numero;	//
 
-    Element element[10];
-} List;
+    Elemento Elemento[MAX];
+} Lista;
 
-int ListaCheia(List *list);
-void Insercao(List *list, Element *x, int cr);
+void FormatarTexto(char texto[]) {
+	int quat = strlen(texto);
+	int letra;
+	
+	for( letra=0; letra<quat ; letra++) {
+		texto[letra] = tolower(texto[letra]);
+	}
+}
+
+void iniciarLista(Lista *lista) {
+	lista->inicio=0; 
+    lista->final=0;	
+    lista->no=0;	
+    lista->numero=0;
+}
+
+//void Insercao(List *list, Element *x, int cr);
 
 void main(void)
 {
 
-    List *list;
-    Element *w, *x;
+    Lista *lista;
+    Elemento *w, *x;
     int cr;
 
     int chooseOpcion;
 
-    list = (List *)calloc(1, sizeof(List));
-    w = (Element *)calloc(1, sizeof(Element));
-    x = (Element *)calloc(1, sizeof(Element));
+    lista = (Lista *)calloc(1, sizeof(Lista));
+    w = (Elemento *)calloc(1, sizeof(Elemento));
+    x = (Elemento *)calloc(1, sizeof(Elemento));
 
-    do
-    {
-        fflush(stdin);
-
-        printf("1 - Inserir um valor na lista\n");
-        printf("2 - Excluir um valor na lista\n");
-        printf("3 - Verificar se a lista esta cheia\n");
-        printf("4 - Verificar se a lista esta vazia ou se o elemento a ser excluido nao existe\n");
-        printf("9999 - para sair do programa\n");
-
-        scanf("%i", &chooseOpcion);
-
-        switch (chooseOpcion)
-        {
-        case 1:
-            Insercao(list, x, cr);
-            break;
-        case 2:
-            // colocar a funÃ§Ã£o de excluir
-            break;
-        case 3:
-            ListaCheia(list);
-            break;
-        case 4:
-            // colocar a funÃ§Ã£o de veridicar se a lista estÃ¡ vazia ou se o elemento nÃ£o existe
-            break;
-
-        default:
-            printf("Opcao nao encontrada. Digite outra opcao\n");
-            break;
-        }
-        
-
-    } while (chooseOpcion != 9999);
-
-    system("pause");
+    int dados[MAX],ELO,tipoID=0;
+	char comando[MAX], tipo[3][6];
+	
+	strcpy(tipo[0],"lista");
+	strcpy(tipo[1],"fila");
+	strcpy(tipo[2],"pilha");
+	
+	printf("Escolha o tipo de organização de dados que deseja montar: lista, fila e pilha\n");
+	do{
+		scanf("%s",comando);
+		FormatarTexto(comando);
+		
+	}while( strcmp(comando,tipo[0]) && strcmp(comando,tipo[1]) && strcmp(comando,tipo[2]) );
+	
+	if( !strcmp(comando,tipo[0]) ) {//entra e sai de qualquer lugar
+		printf("Uma lista\n");
+		tipoID = 0;
+		
+	}
+	else {
+		if( !strcmp(comando,tipo[1]) ) {//entra no primeiro Indice e sai no ultimo indereço
+			printf("Uma fila\n");
+			tipoID = 1;
+			
+		}
+		else {
+			if( !strcmp(comando,tipo[2]) ) {//entra e sai na mesma posição
+				printf("Uma pilha\n");
+				tipoID = 2;
+				
+			}
+		}
+	}
+	
+	printf("Escolha o comando para sua %s ou incerrar o programa: fim, buscar e adicionar\n",tipo[tipoID]);
+	do{
+		scanf("%s",comando);
+		FormatarTexto(comando);
+		
+	}while( strcmp(comando,"fim") );
 }
 
-int ListaCheia(List *list)
-{
-    if (list->num != 10)
-    {
-        return 0;
-        printf("A lista nÃ£o esta cheia.\n");
-    }
-    else
-    {
-        return 1;
-        printf("A lista esta cheia.\n");
-    }
-}
-
-void Insercao(List *list, Element *x, int cr)
+/*void Insercao(List *list, Element *x, int cr)
 {
     if (ListaCheia(list) == 1)
     {
@@ -110,4 +117,4 @@ void Insercao(List *list, Element *x, int cr)
 
         list->num += 1; // aqui acrescenta mais um numero na lista, para dizer quantos tem
     }
-}
+}*/
